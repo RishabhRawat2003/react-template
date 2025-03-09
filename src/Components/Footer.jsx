6ad8cb44bb629e89d136fea947fbcd5cfa27264c
Rishabh-Truwix <rishabhr@truwix.com>
2025-03-09 11:40:24 +0530

    template setup

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full h-60 bg-gray-400'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='w-full h-screen'>Homepage</div>
+  )
+}
+
+export default Homepage
